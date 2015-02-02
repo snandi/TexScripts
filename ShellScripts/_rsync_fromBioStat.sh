48e54e2152ddc1591cb93de2aa06a81604e15585
snandi <nandi@stat.wisc.edu>
2015-02-02 12:15:20 -0600

    Added rsync commands

---
+++ ShellScripts/_rsync_fromBioStat.sh
@@ -0,0 +1,3 @@
+rsync -av ~/Courses/Stat771_Spring2015/ nandi@desk00.stat.wisc.edu:Courses/Stat771_Spring2015 
+rsync -av ~/Courses/Stat771_Spring2015/ nandi@jupiter.lmcg.wisc.edu:Courses/Stat771_Spring2015 
+
---
+++ ShellScripts/_rsync_fromStat.sh
@@ -0,0 +1,3 @@
+rsync -av ~/Courses/Stat771_Spring2015/ snandi@adhara.biostat.wisc.edu:Courses/Stat771_Spring2015
+rsync -av ~/Courses/Stat771_Spring2015/ nandi@jupiter.lmcg.wisc.edu:Courses/Stat771_Spring2015 
+
