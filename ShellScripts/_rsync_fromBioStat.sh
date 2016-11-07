ab22ebdcf4cae7b9ab8f1f7801b5593ffb6e425c
snandi <nandi@stat.wisc.edu>
2016-11-06 20:00:28 -0600

    Updated rsync command

--- ShellScripts/_rsync_fromBioStat.sh
+++ ShellScripts/_rsync_fromBioStat.sh
@@ -1,3 +1,5 @@
-rsync -av ~/Courses/Stat771_Spring2015/ nandi@desk00.stat.wisc.edu:Courses/Stat771_Spring2015 
-rsync -av ~/Courses/Stat771_Spring2015/ nandi@jupiter.lmcg.wisc.edu:Courses/Stat771_Spring2015 
+find . -type f -name '*~' -delete
+
+rsync -av ~/Project_BAC/ nandi@desk00.stat.wisc.edu:Project_BAC --delete-before
+rsync -av ~/Project_BAC/ nandi@jupiter.lmcg.wisc.edu:Project_BAC --delete-before
 
